eb3a1f9d6ee83396d9174892731987bebcaabe7f
alex-stanbridge <acameron@stanbridge.edu>
2026-02-27 20:53:03 -0800

    Fix npm issues.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  root: "public",
-
   plugins: [svelte(), tailwindcss()],
 
   server: {
